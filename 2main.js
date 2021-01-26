e8f3dced5488adedd6119059752c71d892d47859
Nanase <64364271+Namane00426@users.noreply.github.com>
2021-01-26 18:04:54 +0100

    practice #2 greetAliens()

---
+++ 2main.js
@@ -0,0 +1,14 @@
+// Write your code here:
+const greetAliens = (arr) => {
+  for (string of arr){
+    console.log(`Oh powerful ${string}, we humans offer our unconditional surrender!`);
+  }
+}
+
+
+
+// When you're ready to test your code, uncomment the below and run:
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);
